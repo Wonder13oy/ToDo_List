c6d5fd04a5301cb7275a3247fc8ed1ee704e9710
Wonder13oy <wandilenxumalo131@gmail.com>
2019-04-05 09:34:56 +0200

    This is the first commit

---
+++ backend.js
@@ -0,0 +1,9 @@
+//Making the to do list work
+function todoList() {
+  let item = document.getElementById('todoInput').value;
+  let text = document.createTextNode(item);
+  let newItem = document.createElement('li');
+  newItem.appendChild(text);
+
+  document.getElementById('todoList').appendChild(newItem);
+}
